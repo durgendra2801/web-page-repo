836cf1ab2a4a706aa0078073d15512b2486600a6
durgendra2801 <durgendra4ever@gmail.com>
2021-02-18 21:01:10 +0530

    project

---
+++ SprWeb/src/main/java/com/durgendra/SprWebApplication.java
@@ -0,0 +1,13 @@
+package com.durgendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprWebApplication.class, args);
+	}
+
+}
---
+++ SprWeb/src/test/java/com/durgendra/SprWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgendra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
